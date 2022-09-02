51d6abef912e52841b519126cafd29510e688200
Wei Liao <liaoweithu@gmail.com>
2022-09-02 15:40:47 -0400

    Extraction code compile

---
+++ extract_mimic.py
@@ -0,0 +1,5 @@
+# Set up Google big query
+from google.colab import auth
+auth.authenticate_user()
+from google.colab import drive
+drive.mount('/content/drive')
---
+++ main.py
@@ -0,0 +1,22 @@
+import argparse
+import extract_mimic
+import extract_eicu
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Parse to query MIMIC/eICU data")
+    parser.add_argument("--database", type=str, default='MIMIC', choices=['MIMIC', 'eICU'])
+    parser.add_argument("--age_min", type=int, default=18, help='Min patient age to query')
+    parser.add_argument("--los_min", type=int, default=24, help='Min ICU LOS in hour')
+    parser.add_argument("--los_max", type=int, default=240, help='Max ICU LOS in hour')
+    parser.add_argument("--exit_point", type=str, default='All', choices=['All', 'Raw'],
+                        help='Where to stop the pipeline')
+    parser.add_argument("--patient_group", type=str, default='None', choices=['None', 'sepsis-3'],
+                        help='Specific groups to extract')
+    args = parser.parse_args()
+    if args.database == 'MIMIC':
+        extract_mimic(args)
+    elif args.database == 'eICU':
+        extract_eicu(args)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
